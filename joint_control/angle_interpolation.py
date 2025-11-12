054fa740de7e25758a66c49fcf7cefb8843d6f71
florian01 <flo.t.f@t-online.de>
2025-11-12 14:58:39 +0100

    interpolation fertig

--- joint_control/angle_interpolation.py
+++ joint_control/angle_interpolation.py
@@ -21,7 +21,7 @@
 
 
 from pid import PIDAgent
-from keyframes import hello
+from keyframes import hello, leftBackToStand, leftBellyToStand, rightBackToStand, rightBellyToStand, wipe_forehead
 
 
 class AngleInterpolationAgent(PIDAgent):
@@ -36,9 +36,28 @@ class AngleInterpolationAgent(PIDAgent):
 
     def think(self, perception):
         target_joints = self.angle_interpolation(self.keyframes, perception)
-        target_joints['RHipYawPitch'] = target_joints['LHipYawPitch'] # copy missing joint in keyframes
+        if 'LHipYawPitch' in target_joints:
+            target_joints['RHipYawPitch'] = target_joints['LHipYawPitch'] # copy missing joint in keyframes
         self.target_joints.update(target_joints)
         return super(AngleInterpolationAgent, self).think(perception)
+
+    def bezier_interpolation(self, i, p0, p1, p2, p3):
+        return (((1-i)**3) * p0) + (3 * ((1- i)**2) * i * p1)+ (3 * (1- i) * (i**2) * p2) + ((i**3) * p3)  
+
+    def control_points(self, a0, a1, next_hendl, prev_hendl):
+        offset_output = (next_hendl[2] if next_hendl is not None else 0.0)
+        offset_prev = (prev_hendl[2] if prev_hendl is not None else 0.0)
+        p0 = a0 # startwinkel
+        p1 = a0 + float(offset_output) # verschobener winkel
+        p2 = a1 + float(offset_prev)
+        p3 = a1 # outputwinkel
+        return p0, p1, p2, p3
+
+    def find_segment(self, time_joint_list, time_now):
+            for i in range(len(time_joint_list) - 1):
+                if time_joint_list[i] <= time_now < time_joint_list[i+1]:
+                    return i
+            return max(0, len(time_joint_list)-2)
 
     def angle_interpolation(self, keyframes, perception):
         target_joints = {}
@@ -51,28 +70,40 @@ class AngleInterpolationAgent(PIDAgent):
             self.start_time = perception.time
         relative_time = perception.time - self.start_time
 
-        for i, joints in enumerate(names):
-            jframes = keys[i]
-            jtimes = times[i]
+        for i, joint_name in enumerate(names):
+            key_list = keys[i]
+            time_list = times[i]
 
-            if not jtimes:
+            if not time_list:
                 continue
-            
-            angle, prev_handle, next_handle = [], [] , []
-            for j in jframes:
 
+            if relative_time <= time_list[0]: #vor erstem Keyframe
+                target_joints[joint_name] = key_list[0][0]
+                continue
+            if relative_time >= time_list[-1]: #nach letztem keyframe
+                target_joints[joint_name] = key_list[-1][0]
+                continue
+            #find segment
+            tmp = self.find_segment(time_list, relative_time)
+            start, end = time_list[tmp], time_list[tmp+1]
+            normalisied_time = (relative_time - start) / (end - start) if (end > start) else 0.0
 
-            
+            #Hendlers + KEyframes
+            a0, prev_hendle_0, next_hendler_0 = key_list[tmp]
+            a1, prev_hendle_1, next_hendler_1 = key_list[tmp + 1]
 
+            #Kontrollpunkte
+            p0, p1, p2, p3 = self.control_points(a0, a1, next_hendler_0, prev_hendle_1)
+
+            Bezier = self.bezier_interpolation(normalisied_time, p0, p1, p2, p3)
+
+            target_joints[joint_name] = Bezier
 
         return target_joints
-
-def bezier_interpolation(i, p0, p1, p2, p3):
-    return (((1-i)**3) * p0) + (3 * ((1- i)**2) * i * p1)+ (3 * (1- i) * (i**2) * p2) + ((i**3) * p3)  
-
 
 
 if __name__ == '__main__':
     agent = AngleInterpolationAgent()
-    agent.keyframes = hello()  # CHANGE DIFFERENT KEYFRAMES
+    #agent.keyframes = hello()  # CHANGE DIFFERENT KEYFRAMES
+    agent.keyframes = wipe_forehead(motion="left")
     agent.run()
